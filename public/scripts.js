be7154265202e811cef6f6bca90f08de12c75598
Mark Carey <marcar2005@hotmail.co.uk>
2025-07-15 06:28:35 +0100

    rebasing isualiser

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
 const stopStreamBtn = document.getElementById("stopStreamBtn")
 const startStreamBtn = document.getElementById("startStreamBtn")
+
+const audio = document.getElementById("stream")
+const canvas = document.getElementById("visualiser")
+const ctx = canvas.getContext('2d')
+
+const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+const analyser = audioCtx.createAnalyser();
+analyser.fftSize = 256;
+
+const source = audioCtx.createMediaElementSource(audio);
+source.connect(analyser);
+analyser.connect(audioCtx.destination);
+
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+
+
+function drawVisualiser(){
+    requestAnimationFrame(drawVisualiser);
+
+    analyser.getByteFrequencyData(dataArray)
+
+    ctx.fillStyle = '#111'
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    
+    const barWidth = (WIDTH / bufferLength) * 2.5;
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+      const barHeight = dataArray[i];
+
+      ctx.fillStyle = `rgb(${barHeight + 100},50,50)`;
+      ctx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+
+      x += barWidth + 1;
+    }
+  }
+
+  drawVisualiser()
+
+
+
 stopStreamBtn.addEventListener('click', () =>{
     fetch('/stop-stream', {
         method: 'POST'
