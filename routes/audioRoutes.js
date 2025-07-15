ca6bd0c78b4857a8a33d11d0c661e270b017e939
markC2 <99101684+markC2@users.noreply.github.com>
2025-07-15 06:12:42 +0100

    Experimenting with hls chunked streaming (#6)
    * added hls streaming with aa new hls handler and killStream and startStream helper functions binded to two buttons in UI for start/stopping via a POST request to (start,stop)-stream. also added automatic purging of stream folder contents when stream is stopped to avoid buildup.
    
    * added stream folder to gitignore

--- routes/audioRoutes.js
+++ routes/audioRoutes.js
@@ -1,10 +1,17 @@
 //audioRoutes 
 const express = require ('express')
 
-const audioController = require('../controllers/audioController')
+const {audioControllerRawMp3,HLSstreamHandler} = require('../controllers/audioController')
+
+
 const router = express.Router()
 
-router.get('/audio', audioController)
+//for streaming raw mp3 not hls chunked
+router.get('/audio', audioControllerRawMp3)
+
+router.post('/stop-stream', HLSstreamHandler)
+
+router.post('/start-stream', HLSstreamHandler)
 
 
 module.exports = router
--- services/FFMPEGService.js
+++ services/FFMPEGService.js
@@ -1,11 +1,13 @@
 //ffmpegService
 const { spawn } = require('child_process');
-
+const path = require('path');
+const fs = require('fs');
 
 let ffmpegProcess = null;
 let audioClients = [];
+const outputDir = path.resolve(__dirname, '../public/stream')
 
-function  spawnFFMPEGMP3Stream(device){
+function  spawnFFMPEGMP3StreamRaw(device){
 
     //singleton
     if(ffmpegProcess) return;
@@ -36,9 +38,69 @@ function  spawnFFMPEGMP3Stream(device){
     });
 }
 
+
+function  spawnFFMPEGMP3StreamHLS(device){
+
+    //singleton
+    if(ffmpegProcess) return;
+
+   
+
+    ffmpegProcess = spawn('ffmpeg', [
+        '-f', 'avfoundation', // macOS only — change if on Linux/Windows
+        '-i', `:${device}`,
+        '-c:a', 'aac',
+        '-b:a', '128k',
+        '-f', 'hls',
+        '-hls_time', '5',
+        '-hls_list_size', '6',
+        '-hls_flags', 'delete_segments',
+        `${outputDir}/stream.m3u8`
+      ]);
+
+      ffmpegProcess.stderr.on('data', data => {
+        console.error('FFmpeg:', data.toString());
+      });
+    
+      ffmpegProcess.on('exit', () => {
+        console.log(' FFmpeg stopped streaming');
+        ffmpegProcess = null;
+        cleanUpStreamFiles()
+      });
+    
+      console.log(' Started HLS stream to /public/stream');
+
+
+}
+
+
+function cleanUpStreamFiles(){
+    fs.readdir(outputDir, (err, files)=>{
+        if (err) return console.error("failed to read stream dir:", err)
+
+        files.forEach(file => {
+            if (file.endsWith(".ts") || file.endsWith(".m3u8")){
+                fs.unlink(path.join(outputDir, file), err => {
+                    if(err) console.error("failed to delete", file, err)
+
+                })
+            }
+        })
+
+        console.log('cleaned up old stream files')
+    })
+}
     
     
-    
+ function killStream(res){
+    if (ffmpegProcess){
+        ffmpegProcess.kill('SIGINT');
+        res.send("Stream Stopped")
+    }
+    else{
+        res.send("Stream not running")
+    }
+ }   
 
 
 function addClient(res){
@@ -48,8 +110,12 @@ function addClient(res){
     });
 
 }
+
+function startStream(res){
+    spawnFFMPEGMP3StreamHLS(process.argv[2])
+}
    
 
 
 
-module.exports = {spawnFFMPEGMP3Stream, addClient}
+module.exports = {spawnFFMPEGMP3StreamRaw,spawnFFMPEGMP3StreamHLS, addClient, killStream, startStream}
