0bd0e93f23054130685e7bd678587ee432bbfaf6
Mark Carey <marcar2005@hotmail.co.uk>
2025-07-15 10:52:07 +0100

    removing source, hls dynamically adds one

--- public/scripts.js
+++ public/scripts.js
@@ -2,47 +2,52 @@ document.addEventListener('DOMContentLoaded', () => {
 const stopStreamBtn = document.getElementById("stopStreamBtn")
 const startStreamBtn = document.getElementById("startStreamBtn")
 
-const audio = document.getElementById("stream")
-const canvas = document.getElementById("visualiser")
-const ctx = canvas.getContext('2d')
 
-const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-const analyser = audioCtx.createAnalyser();
-analyser.fftSize = 256;
-
-const source = audioCtx.createMediaElementSource(audio);
-source.connect(analyser);
-analyser.connect(audioCtx.destination);
-
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
-
-const WIDTH = canvas.width;
-const HEIGHT = canvas.height;
-
-
-function drawVisualiser(){
-    requestAnimationFrame(drawVisualiser);
-
-    analyser.getByteFrequencyData(dataArray)
-
-    ctx.fillStyle = '#234'
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+function setupAudioVisualiser(audio){
+    console.log("🔊 Setting up visualizer"); 
+    const canvas = document.getElementById("visualiser")
+    const ctx = canvas.getContext('2d')
     
-    const barWidth = (WIDTH / bufferLength) * 2.5;
-    let x = 0;
+    const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 256;
+    
+    const source = audioCtx.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+    
+    
+    function drawVisualiser(){
+        requestAnimationFrame(drawVisualiser);
+    
+        analyser.getByteFrequencyData(dataArray)
+    
+        ctx.fillStyle = '#234'
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        
+        const barWidth = (WIDTH / bufferLength) * 2.5;
+        let x = 0;
+    
+        for (let i = 0; i < bufferLength; i++) {
+          const barHeight = dataArray[i];
+    
+          ctx.fillStyle = `rgb(${barHeight + 100},50,50)`;
+          ctx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+    
+          x += barWidth + 1;
+        }
+      }
+    
+      drawVisualiser()
 
-    for (let i = 0; i < bufferLength; i++) {
-      const barHeight = dataArray[i];
+}
 
-      ctx.fillStyle = `rgb(${barHeight + 100},50,50)`;
-      ctx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
-
-      x += barWidth + 1;
-    }
-  }
-
-  drawVisualiser()
 
 
 
@@ -81,22 +86,25 @@ startStreamBtn.addEventListener('click', () =>{
 
 
 
+  const streamAudio = document.getElementById("stream")
   const streamSrc = '/stream/stream.m3u8';
 
   if (Hls.isSupported()) {
     const hls = new Hls();
     hls.loadSource(streamSrc);
-    hls.attachMedia(audio);
+    hls.attachMedia(streamAudio);
     hls.on(Hls.Events.MANIFEST_PARSED, () => {
-      audio.play();
+      streamAudio.play().then(() =>{
+        setupAudioVisualiser(streamAudio)
+      });
     });
-  } else if (audio.canPlayType('application/vnd.apple.mpegurl')) {
+  } else if (streamAudio.canPlayType('application/vnd.apple.mpegurl')) {
     // Safari fallback
-    audio.src = streamSrc;
-    audio.addEventListener('loadedmetadata', () => {
-      audio.play();
+    streamAudio.src = streamSrc;
+    streamAudio.addEventListener('loadedmetadata', () => {
+      streamAudio.play();
     });
   } else {
-    console.error('❌ HLS is not supported in this browser.');
+    console.error(' HLS is not supported in this browser.');
   }
 })
