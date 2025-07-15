4b0f1d272b8d9209e0a034740143f86751cc7575
markC2 <99101684+markC2@users.noreply.github.com>
2025-07-15 06:50:22 +0100

    added in support for HLS on chrome (#7)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const audioRoutes = require('./routes/audioRoutes');
 
 app.use(express.static('public'));
 app.use('/', audioRoutes)
-app.use('/stream', express.static('public/stream'));
+app.use('/stream', express.static('/public/stream'));
 
 
 module.exports = {app}
